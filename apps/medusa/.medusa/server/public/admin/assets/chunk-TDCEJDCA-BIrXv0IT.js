a3d21565d8f8aebc13d02a53c293ed78d7e1078f
DuanYu <duanyu-fs@DuanYu-FS.local>
2025-11-03 00:10:38 +0700

    rebuild

--- apps/medusa/.medusa/server/public/admin/assets/chunk-TDCEJDCA-BIrXv0IT.js
+++
@@ -1,1 +0,0 @@
-import{aQ as i}from"./index-BHu4VF78.js";var a=i("product","*categories,*shipping_profile,-variants");export{a as P};
---
+++ apps/medusa/.medusa/server/public/admin/assets/chunk-TDCEJDCA-XY9o7CXy.js
@@ -0,0 +1,1 @@
+import{aQ as i}from"./index-Dwx6bRsG.js";var a=i("product","*categories,*shipping_profile,-variants");export{a as P};
