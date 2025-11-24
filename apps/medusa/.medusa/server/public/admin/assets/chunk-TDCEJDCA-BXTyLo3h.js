5502fed1f925484f33cf769c609e0d16007acd32
DuanYu <duanyu-fs@192.168.2.69>
2025-11-24 14:34:28 +0700

    feat: rebuild

--- apps/medusa/.medusa/server/public/admin/assets/chunk-TDCEJDCA-BXTyLo3h.js
+++
@@ -1,1 +0,0 @@
-import{aP as i}from"./index-DIHdb3v6.js";var a=i("product","*categories,*shipping_profile,-variants");export{a as P};
---
+++ apps/medusa/.medusa/server/public/admin/assets/chunk-TDCEJDCA-C7cn4DeC.js
@@ -0,0 +1,1 @@
+import{aP as i}from"./index-d0TOaYYd.js";var a=i("product","*categories,*shipping_profile,-variants");export{a as P};
