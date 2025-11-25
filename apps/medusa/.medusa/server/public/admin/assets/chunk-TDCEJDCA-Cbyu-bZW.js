8cac54c963482bcfe1b0efb330a12e9b6579a890
DuanYu <duanyu-fs@DuanYu-FS.local>
2025-11-25 10:32:38 +0700

    rebuild

---
+++ apps/medusa/.medusa/server/public/admin/assets/chunk-TDCEJDCA-Cbyu-bZW.js
@@ -0,0 +1,1 @@
+import{aP as i}from"./index-CgJZ6uNO.js";var a=i("product","*categories,*shipping_profile,-variants");export{a as P};
--- apps/medusa/.medusa/server/public/admin/assets/chunk-TDCEJDCA-CkVbvCD3.js
+++
@@ -1,1 +0,0 @@
-import{aP as i}from"./index-Dl-EK_qD.js";var a=i("product","*categories,*shipping_profile,-variants");export{a as P};
