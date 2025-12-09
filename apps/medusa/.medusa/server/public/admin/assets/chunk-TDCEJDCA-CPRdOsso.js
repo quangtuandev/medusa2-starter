33c6476bff806c6d74e99407149366fe84d658b7
DuanYu <duanyu-fs@192.168.2.69>
2025-12-10 02:50:31 +0700

    fix paypal

---
+++ apps/medusa/.medusa/server/public/admin/assets/chunk-TDCEJDCA-CPRdOsso.js
@@ -0,0 +1,1 @@
+import{aQ as i}from"./index-C4Dnqr8E.js";var a=i("product","*categories,*shipping_profile,-variants");export{a as P};
--- apps/medusa/.medusa/server/public/admin/assets/chunk-TDCEJDCA-Dmtd3Oyf.js
+++
@@ -1,1 +0,0 @@
-import{aQ as i}from"./index-ClFP-60W.js";var a=i("product","*categories,*shipping_profile,-variants");export{a as P};
