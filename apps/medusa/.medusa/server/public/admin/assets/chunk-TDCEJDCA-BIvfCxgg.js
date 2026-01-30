3c8d8fba8cd0191bf46323d116763cacd152e080
Quang Tuan <quangtuandev@gmail.com>
2026-01-30 09:44:00 +0700

    rebuild

---
+++ apps/medusa/.medusa/server/public/admin/assets/chunk-TDCEJDCA-BIvfCxgg.js
@@ -0,0 +1,1 @@
+import{aQ as i}from"./index-CDfO9x5H.js";var a=i("product","*categories,*shipping_profile,-variants");export{a as P};
--- apps/medusa/.medusa/server/public/admin/assets/chunk-TDCEJDCA-awiNyu5G.js
+++
@@ -1,1 +0,0 @@
-import{aQ as i}from"./index-CTY5BQTe.js";var a=i("product","*categories,*shipping_profile,-variants");export{a as P};
